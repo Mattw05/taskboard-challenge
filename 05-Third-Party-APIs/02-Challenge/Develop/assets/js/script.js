cd006cd7cd6c1e049b78cfec04e01b85cca17437
Mya Weigandt <myaweigandt@Myas-MacBook-Air.local>
2024-07-01 20:21:27 -0400

    made task list an array

--- 05-Third-Party-APIs/02-Challenge/Develop/assets/js/script.js
+++ 05-Third-Party-APIs/02-Challenge/Develop/assets/js/script.js
@@ -1,23 +1,23 @@
 // Retrieve tasks and nextId from localStorage
-let taskList = JSON.parse(localStorage.getItem("tasks"));
+let taskList = JSON.parse(localStorage.getItem("tasks")) || [];
 let nextId = JSON.parse(localStorage.getItem("nextId"));
-
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
-    return taskList.length > 0 ? Math.max(...taskList.map(task => task.id)) + 1 : 1;
+  return taskList.length > 0
+    ? Math.max(...taskList.map((task) => task.id)) + 1
+    : 1;
 }
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
+  const taskCard = document.createElement("div");
+  taskCard.classList.add("task-card");
+  taskCard.setAttribute("data-task-id", task.id);
+  taskCard.draggable = true;
+  setColor(taskCard, task);
 
-    const taskCard = document.createElement("div");
-    taskCard.classList.add('task-card');
-    taskCard.setAttribute('data-task-id', task.id);
-    taskCard.draggable = true;
-    setColor(taskCard, task);
-    
-    taskCard.innerHTML = `
+  taskCard.innerHTML = `
     <div class="card mb-1">
         <div class="card-header">${task.title}</div>
         <div class="card-body">
@@ -28,94 +28,76 @@ function createTaskCard(task) {
     </div>
 `;
 
-    document.getElementById('todo-cards').appendChild(taskCard);
+  document.getElementById("todo-cards").appendChild(taskCard);
 
-  return taskCard
-
-
-
+  return taskCard;
 }
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
+  // Clear the exisiting cards
+  document.querySelectorAll("task-card").forEach((card) => remove());
 
-    // Clear the exisiting cards
-    document.querySelectorAll('task-card').forEach(card => remove());
+  // Rener task cards
+  let taskCol;
+  if (task.status === "to-do") {
+    taskCol = document.getElementById("to-do");
+  } else if (task.status === "in-progress") {
+    taskCol = document.getElementById("in-progress");
+  } else if (task.status === "done") {
+    taskCol = document.getElementById("done");
+  }
 
-    // Rener task cards
-    let taskCol;
-    if(task.status === 'to-do'){
-        taskCol = document.getElementById('to-do');
-    } else if (task.status === 'in-progress'){
-        taskCol = document.getElementById('in-progress');
-    } else if(task.status === 'done'){
-        taskCol = document.getElementById('done');
-    }
+  // check if swim lane container exists
+  if (taskCol) {
+    // Create task card
+    const taskCard = createTaskCard(task);
 
-
-    // check if swim lane container exists
-    if(taskCol){
-
-        // Create task card
-        const taskCard = createTaskCard(task);
-
-        // Append
-        taskCol.querySelector('.card-body').appendChild(taskCard);
-    }else {
-        console.log('no task found ')
-    }
-
-    
-
-
-    }
-    
-
-
+    // Append
+    taskCol.querySelector(".card-body").appendChild(taskCard);
+  } else {
+    console.log("no task found ");
+  }
+}
 
 // Todo: create a function to handle adding a new task
-function handleAddTask(event){
-// Prevent derfault behavior
-event.preventDefualt();
+function handleAddTask(event) {
+  // Prevent derfault behavior
+  event.preventDefualt();
 
-// Get form inputs
-const title = document.getElementById('task-title').value;
-const dueDate = document.getElementById('task-due-date').value;
-const description = document.getElementById('task-description').value;
+  // Get form inputs
+  const title = document.getElementById("task-title").value;
+  const dueDate = document.getElementById("task-due-date").value;
+  const description = document.getElementById("task-description").value;
 
-// Create a mew tasl object
-const newTask = {
+  // Create a mew tasl object
+  const newTask = {
     id: generateTaskId(), // generates a unique id
     title: title,
     dueDate: dueDate,
     description: description,
-    status: 'to-do', // sets the initial value
-};
+    status: "to-do", // sets the initial value
+  };
 
+  // Add new task to the taskList array
+  taskList.push(newTask);
 
-// Add new task to the taskList array
-taskList.push(newTask);
+  // Update local storage
+  localStorage.setItem("tasks", JSON.stringify(taskList));
 
-// Update local storage
-localStorage.setItem('tasks', JSON.stringify(taskList));
+  renderTaskList();
 
-renderTaskList();
-
-// Hide from after rendering task 
-$(`#formModal`).modal('hide');
+  // Hide from after rendering task
+  $(`#formModal`).modal("hide");
 }
+
+console.log(taskList);
 
 // Todo: create a function to handle deleting a task
-function handleDeleteTask(event){
-
-}
+function handleDeleteTask(event) {}
 
 // Todo: create a function to handle dropping a task into a new status lane
-function handleDrop(event, ui) {
-
-}
+function handleDrop(event, ui) {}
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
-$(document).ready(function () {
-
-});
+$(document).ready(function () {});
